274627dfdfb276721d2f8c6012c11b46b45acad0
Eduarda Luzia Brezinski <brezinski.eduarda@gmail.com>
2025-09-07 14:45:00 -0300

    faker

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,19 +1,32 @@
+import { faker } from '@faker-js/faker';
+
+
 describe('Central de Atendimento ao Cliente TAT', () => {
     beforeEach(() => {
         cy.visit('./src/index.html')
+        const user = {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            longText: faker.lorem.paragraphs(3) //gera textos mais realistas 
+        }
+        cy.wrap(user).as('user') //alias para utilizar em todo o sistema
     })
 
     it('verifica o título da aplicação', () => {
-
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
     it('preenche os campos obrigatórios e envia do formulário', () => {
-        cy.get('#firstName').type('Maria')
-        cy.get('#lastName').type('Oliveira')
-        cy.get('#email').type('m.oli@mail.com')
-        cy.get('#open-text-area').type('Texto padrão de texte - Preencher campos obrigatórios')
+        cy.get('#firstName').type(this.user.firstName)
+        cy.get('#lastName').type(this.user.lastName)
+        cy.get('#email').type(this.user.email)
+        cy.get('#open-text-area').type(this.user.longText, { delay: 0 })
         cy.get('button[type="submit"]').click()
-
         cy.get('.success').should('be.visible')
+    })
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+
+
+
     })
 })
